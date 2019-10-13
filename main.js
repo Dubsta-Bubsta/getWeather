62e0f053cf9fc14dfb49f2b841c21b8ad9c46b7f
Dubsta <dubsta.post@yandex.ru>
2019-10-14 01:00:43 +0300

    All project
    Working with simple api

---
+++ main.js
@@ -0,0 +1,24 @@
+document.querySelector('.button-primary').addEventListener('click', () => {
+	let chosenCity = document.querySelector('select').value;
+	document.querySelector('.weather-table').style.display = "block";	
+
+	//Making request to openweathermap api. APi id = 053d12d7ddb982ab5795c13f6aa222f5
+	fetch(`http://api.openweathermap.org/data/2.5/weather?q=${chosenCity}&appid=053d12d7ddb982ab5795c13f6aa222f5`)
+	.then(function (resp){
+		return resp.json()	//return json data string
+	})
+	.then(function (data){
+		console.log(data);	//Filling data to html
+		document.querySelector('.city-name').textContent = `Город:  ${data.name}`;
+		document.querySelector('.temperature').innerHTML = `Temperature: ${Math.round(data.main.temp - 273) + '&deg;'}C`;
+		document.querySelector('.pressure').textContent = `Pressure:  ${data.main.pressure * 0.75} mm Hg`;
+		document.querySelector('.description').innerHTML = `Sky: ${data.weather[0]['description']}`;
+		document.querySelector('.weather-logo').innerHTML = `<img src = "https://openweathermap.org/img/wn/${data.weather[0]['icon']}@2x.png">`;
+	})
+	.catch(function() {
+		console.log('Error!')
+	});	
+});
+
+
+
